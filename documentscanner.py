3374af4db30032b4a010ab6c225f7a880c4f87f0
Tadinma Chukwuneme <officiallychuks@gmail.com>
2019-08-20 19:46:41 -0700

    remove command line interface

--- documentscanner.py
+++ documentscanner.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import sys
 
-ap = argparse.ArgumentParser()
+"""ap = argparse.ArgumentParser()
 ap.add_argument("-i,","--image",required=True,help="Path to input image")
-args = vars(ap.parse_args())
+args = vars(ap.parse_args())"""
 
 
-image = cv2.imread(args["image"]) #read image
+image = cv2.imread("doc1.jpg") #read image
 print(image)
 
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #convert image to gray
@@ -16,12 +17,8 @@ gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) #convert image to gray
 ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) #binarize the image
 imh, cnts, heir = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) 
 
-#find shapes in the image
-orig = image.copy()
+#sort the contours according to size
 
-#contoured = cv2.drawContours(orig,cnts,-1,(255,0,0),3) #draw the contours on the image
-
-# Sort the contours according to the contour area
 if len(cnts) > 1:
     cnts_sorted = sorted(cnts,key=cv2.contourArea,reverse=True)[:-1]
     cnts_sorted
@@ -35,15 +32,19 @@ sss = cv2.arcLength(r,True)
 approx = cv2.approxPolyDP(r,0.02*sss,True)
 
 
+#make sure our assumption that the document has four corners is satisfied
+
+if len(approx) == 4:
+	top_left = approx[1]
+	top_right = approx[0]
+	bottom_left = approx[2]
+	bottom_right = approx[3]
+else:
+	print("Document not found")
+	sys.exit(1)
+
 
 print("Approx ",approx)
-
-
-top_left = approx[1]
-top_right = approx[0]
-bottom_left = approx[2]
-bottom_right = approx[3]
-
 
 rect = [top_left,top_right,bottom_left,bottom_right]
 rect = np.array(rect,dtype="float32")
@@ -67,3 +68,4 @@ cv2.imshow("warped",warped)
 cv2.imshow("original image",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite("mydoc.jpg",warped)
